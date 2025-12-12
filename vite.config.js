3a846f2d4a8e7e30e6d485d52dfa66e1a51b9d69
unknown <tafitasoaravelonarivo@gmail.com>
2025-12-12 18:45:50 +0300

    viteConfig

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-// ✅ DO THIS - Only expose what you actually need
   define: {
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
   },
@@ -12,5 +11,12 @@ export default defineConfig({
     commonjsOptions: {
       transformMixedEsModules: true,
     }
-  } 
+  },
+
+  // 🔥 AJOUT IMPORTANT POUR RENDER
+  preview: {
+    host: '0.0.0.0',
+    port: 10000,
+    allowedHosts: ['gestion-inata.onrender.com']
+  }
 })
