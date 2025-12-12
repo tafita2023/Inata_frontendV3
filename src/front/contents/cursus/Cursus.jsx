426b1a6b2b0073c26190c61c9c1fab967b4c3077
unknown <tafitasoaravelonarivo@gmail.com>
2025-12-12 18:08:46 +0300

    Correction cursus5

--- src/front/contents/cursus/Cursus.jsx
+++ src/front/contents/cursus/Cursus.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
-import Flow from 'frontend/src/images/cursus.png';
 function Cursus() {
   return (
     <div className="bg-gray-100 flex flex-col items-center p-2">
       <h2 className="text-3xl font-bold text-center text-gray-800 mt-2 mb-2">
         Cursus de Formation
       </h2>
-      <img
-        src={Flow}
-        alt="Cursus"
-        className="max-w-full max-h-full object-contain"
-      />
     </div>
   );
 }
