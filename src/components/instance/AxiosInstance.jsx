4ec757a40ca448a141de35bc36004c80d02141e0
unknown <tafitasoaravelonarivo@gmail.com>
2025-12-13 05:46:58 +0300

    correction page admin2

--- src/components/instance/AxiosInstance.jsx
+++ src/components/instance/AxiosInstance.jsx
@@ -1,15 +1,29 @@
 import axios from 'axios';
 
 const isDevelopment = import.meta.env.MODE === 'development';
-const myBaseUrl = isDevelopment ? import.meta.env.VITE_API_BASE_URL_LOCAL: import.meta.env.VITE_API_BASE_URL_DEPLOY
+const myBaseUrl = isDevelopment 
+  ? import.meta.env.VITE_API_BASE_URL_LOCAL
+  : import.meta.env.VITE_API_BASE_URL_DEPLOY;
 
 const AxiosInstance = axios.create({ 
-    baseURL: myBaseUrl, 
-    timeout: 50000, // 50 secondes de temps d'attente maximum
-    headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-    },
+  baseURL: myBaseUrl, 
+  timeout: 50000, // 50 secondes
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
 });
 
-export default AxiosInstance
+// Intercepteur pour ajouter le token automatiquement
+AxiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default AxiosInstance;
